a6847c3174ffca62f5ac64f44a7125018fd02aed
diana <m000rtualuna@gmail.com>
2026-03-31 21:48:49 +0700

    basic structure

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,15 +2,15 @@
 
 namespace Controller;
 
-use Src\View
+use Src\View;
 class Site
 {
-    public function index(): void
+    public function index(): string
     {
         $view = new View();
         return $view->render('site.hello', ['message' => 'index working']);
     }
-    public function hello(): void
+    public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
