2430e5254f0f292ce3a69400d59d8d60ef5304b2
diana <m000rtualuna@gmail.com>
2026-03-31 21:19:00 +0700

    add layout

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,14 +2,16 @@
 
 namespace Controller;
 
+use Src\View
 class Site
 {
     public function index(): void
     {
-        echo 'working index';
+        $view = new View();
+        return $view->render('site.hello', ['message' => 'index working']);
     }
     public function hello(): void
     {
-        echo 'working hello';
+        return new View('site.hello', ['message' => 'hello working']);
     }
 }
