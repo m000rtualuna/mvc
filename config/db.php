a9299300650251d42e83c947e080948b90e26142
diana <m000rtualuna@gmail.com>
2026-04-01 22:40:58 +0700

    add model

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'MYSQL-8.4',
+    'database' => 'mvc',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
