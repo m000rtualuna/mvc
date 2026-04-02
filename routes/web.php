3bbad553c39bf43d5360c40823f7aef7d53e67b9
diana <m000rtualuna@gmail.com>
2026-04-02 13:03:03 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
 Route::add('GET', '/go', [Controller\Site::class, 'index']);
+// Route::add('GET', '/subscribers', [Controller\Site::class, 'subscribers']);
+// Route::add('GET', '/telephones', [Controller\Site::class, 'telephones']);
+// Route::add('GET', '/rooms', [Controller\Site::class, 'rooms']);
+// Route::add('GET', '/users', [Controller\Site::class, 'users']);
